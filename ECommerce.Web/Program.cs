ba063d9e744d3be90707bcd70a5e131152061449
Mr_Sekiro <abdullah811001@gmail.com>
2025-10-17 10:27:02 +0300

    Product Configurations & Db Context

--- ECommerce.Web/Program.cs
+++ ECommerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace ECommerce.Web
 {
     public class Program
@@ -13,6 +16,11 @@ namespace ECommerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StoreDbContext>(options => 
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
---
+++ Infastructure/Persistence/Data/StoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Product>(new ProductConfigurations());
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+        }
+    }
+}
