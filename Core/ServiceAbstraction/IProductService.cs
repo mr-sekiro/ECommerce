085c7f2bc0a725cadc1c00f0bd5426a1a01e9540
Mr_Sekiro <abdullah811001@gmail.com>
2025-11-09 23:36:39 +0200

    Product Service

---
+++ Core/ServiceAbstraction/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+    public interface IProductService
+    {
+        //get all Products
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        //get all Brands
+        Task<IEnumerable<BrandDto>> GetAllBrandsAsync();
+        //get all Types
+        Task<IEnumerable<TypeDto>> GetAllTypesAsync();
+        //get Product by id
+        Task<ProductDetailsDto?> GetByIdAsync(int id);
+        Task AddAsync(CreateProductDto productDto);
+        Task UpdateAsync(UpdateProductDto productDto);
+        Task DeleteAsync(int id);
+    }
+}
