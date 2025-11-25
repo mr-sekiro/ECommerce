bb4682dad3eb14fb954090e424d17e47fe3234c1
Mr_Sekiro <abdullah811001@gmail.com>
2025-11-25 03:25:58 +0200

    Entities

---
+++ Core/DomainLayer/Models/OrderModels/DeliveryMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModels
+{
+    public class DeliveryMethod : BaseEntity<int>
+    {
+        public string ShortName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string DeliveryTime { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+    }
+}
